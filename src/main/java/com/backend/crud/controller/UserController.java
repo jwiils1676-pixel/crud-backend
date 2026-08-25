78197e3e7a7f9ae2e7c7a8ff277a2d6327e1eb03
john <jwiils1676@gmail.com>
2026-08-24 19:46:58 -0500

    crossOrigin

--- src/main/java/com/backend/crud/controller/UserController.java
+++ src/main/java/com/backend/crud/controller/UserController.java
@@ -18,7 +18,7 @@ import com.backend.crud.dto.userValidationDTOPost;
 import com.backend.crud.dto.userValidationDTOPut;
 
 
-@CrossOrigin(origins = {"http://localhost:4200", "https://crud-backend-xwqd.onrender.com"}, methods = { RequestMethod.GET, RequestMethod.POST, RequestMethod.DELETE,
+@CrossOrigin(origins = {"http://localhost:4200", "https://front-crud-rho.vercel.app"}, methods = { RequestMethod.GET, RequestMethod.POST, RequestMethod.DELETE,
         RequestMethod.PUT })
 
 @RestController
